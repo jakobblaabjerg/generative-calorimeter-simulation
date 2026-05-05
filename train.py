39256018a7ebce85a06e0604289128ff51051831
Jakob Blaabjerg Møller <jblaabjerg@hotmail.com>
2026-05-05 09:41:30 +0200

    new update

--- train.py
+++ train.py
@@ -1,68 +1,48 @@
-from src.config import load_config, save_config
-from src.logger import Logger
-from src.datasets import get_data_loader
-from src.models import MODEL_REGISTRY
-from src.optimizers import create_optimizer
-from src.trainer import Trainer
 import argparse
 
-# TO DO:
-# retrain and training schedule
-
-def run_train(cfg, debug=False):
-
-    print("Loading configuration")
-    logger = Logger(**vars(cfg.logger))
-    run_dir = logger.get_run_dir()
-    cfg.run_dir = run_dir
-    save_config(cfg, run_dir)
-
-    print("Setting up loaders")
-    train_loader = get_data_loader(split="train", **vars(cfg.data_loader))
-    val_loader = get_data_loader(split="val", **vars(cfg.data_loader))
-
-    print("Initializing model")
-    model = MODEL_REGISTRY[cfg.name](cfg.model)
-    optimizer = create_optimizer(model, cfg.optimizer)
-
-    print("Starting training")
-    trainer = Trainer(model, optimizer, run_dir, **vars(cfg.trainer))
-    trainer.fit(train_loader, val_loader, debug=debug)
+from src.config import load_config, override_config
+from src.trainer import run_train
 
 
-if __name__ == "__main__":
+def main():
 
-    # user arguments 
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--model", type=str, default=None)
-    group.add_argument("--cfg_file", type=str, default=None)
+    group.add_argument("--model", type=str)
+    group.add_argument("--cfg_file", type=str)
+
     parser.add_argument("--encoder", type=str, default=None)
     parser.add_argument("--patience", type=int, default=None)
     parser.add_argument("--epochs", type=int, default=None)
     parser.add_argument("--log_dir", type=str, default=None)
-    parser.add_argument("--debug", type=bool, default=False)
+    parser.add_argument("--data_dir", type=str, default=None)
+    parser.add_argument("--num_files", type=int, default=None)
+    parser.add_argument("--debug", action="store_true")
+
     args = parser.parse_args()
 
-    # select model 
     if args.cfg_file is not None:
-        cfg_base = load_config(args.cfg_file)
+        cfg = load_config(args.cfg_file)
     else:
-        cfg_base = load_config(f"configs/base_{args.model}.yaml")
-    
-    # optional default encoder
+        cfg = load_config(f"configs/base_{args.model}.yaml")
+
     if args.encoder and args.model == "cfm":
         cfg_encoder = load_config(f"configs/{args.encoder}_encoder.yaml")
-        cfg_base.model.encoder = cfg_encoder
+        cfg.model.encoder = cfg_encoder
 
-    # additional options 
-    cfg_base.logger.log_dir = args.log_dir or f"logs/{cfg_base.name}"
+    cfg.logger.log_dir = args.log_dir or f"logs/{cfg.name}"
 
-    if args.patience is not None:
-        cfg_base.trainer.patience = args.patience
+    overrides = {
+        "trainer.patience": args.patience,
+        "trainer.epochs": args.epochs,
+        "data_loader.load_dir": args.data_dir,
+        "data_loader.num_files": args.num_files,
+    }
 
-    if args.epochs is not None:
-        cfg_base.trainer.epochs = args.epochs
+    override_config(cfg, overrides)
+    run_train(cfg=cfg, debug=args.debug)
 
-    # train model 
-    run_train(cfg_base, debug=args.debug)
+
+
+if __name__ == "__main__":
+    main()
