faa287ae22332933b11cc5ab341898db50466e05
Jakob Blaabjerg Møller <jblaabjerg@hotmail.com>
2026-05-06 14:30:10 +0200

    refactor

--- sample.py
+++ sample.py
@@ -1,44 +1,42 @@
-from src.config import load_config
-from src.datasets import get_data_loader, get_cond_loader
-from src.models import MODEL_REGISTRY, load_checkpoint
-from src.sampler import Sampler
-import torch
-from src.data_processing import DataProcessor
+from src.config import override_config, load_config
+from src.trainer import run_sampling
 
-#######################
+import argparse
 
-VERSION = 8
-MODEL = "cfm"
+def main():
 
-#######################
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_dir", type=str, required=True)
+    parser.add_argument("--data_dir", type=str, required=True)
+    parser.add_argument("--save_dir", type=str, required=True)
+    parser.add_argument("--config_filters", type=str, default="configs/filters.yaml")
+    parser.add_argument("--config_sampling", type=str, default="configs/sampling.yaml")
+    
+    parser.add_argument("--phi", type=float, default=None)
+    parser.add_argument("--theta", type=float, default=None)
+    parser.add_argument("--e_inc", type=float, default=None)
+    parser.add_argument("--num_samples", type=int, default=None)
 
+    args = parser.parse_args()
 
-print("Loading configuration")
+    overrides = {
+        "data_loader.phi": args.phi,
+        "data_loader.theta": args.theta,
+        "data_loader.e_inc": args.e_inc,
+        "data_loader.num_samples": args.num_samples,
+    }
 
-cfg_base = load_config(f"configs/base_{MODEL}.yaml")
-cfg_filters = load_config("configs/filters.yaml")
-cfg_sampling = load_config("configs/sampling.yaml")
+    cfg_filters = load_config(args.config_filters)
+    cfg_sampling = load_config(args.config_sampling)
+    override_config(cfg_sampling, overrides)
 
-run_dir = f"{cfg_base.logger.log_dir}/version_{str(VERSION)}"
-cfg_version = load_config(f"{run_dir}/config.yaml")
+    run_sampling(
+        model_dir=args.model_dir,
+        data_dir=args.data_dir,
+        save_dir=args.save_dir,
+        cfg_filters=cfg_filters,
+        cfg_sampling=cfg_sampling, 
+    )
 
-
-print("Loading model")
-
-device = torch.device(cfg_sampling.device)
-model = MODEL_REGISTRY[MODEL](cfg_version.model).to(device)
-load_checkpoint(run_dir, model, device)
-
-
-print("Sampling data")
-
-standardize_vars = cfg_version.data_loader.standardize_vars
-load_dir = "data/test6"
-data_processor = DataProcessor(cfg_filters, output_dir=load_dir)
-
-sampler = Sampler(model) 
-for i in range(cfg_sampling.num_files):
-    data_loader = get_cond_loader(standardize_vars=standardize_vars, load_dir=load_dir, **vars(cfg_sampling.data_loader))
-    data, meta = sampler.sample(data_loader)
-    data, meta = data_processor.inverse_transform(data, meta, standardize_vars)
-    data_processor.save_data(data, meta, stage="sampled", file_idx=i+1)
+if __name__ == "__main__":
+    main()
