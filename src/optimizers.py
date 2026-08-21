26fbb278d3ba47e8259d93152ae59ceb97e7943c
Jakob Blaabjerg Møller <jblaabjerg@hotmail.com>
2026-08-21 13:22:27 +0200

    fix optimizer lr

--- src/optimizers.py
+++ src/optimizers.py
@@ -5,21 +5,27 @@ def create_optimizer(model, config):
     if config.type == "adam":
         return torch.optim.Adam(
             model.parameters(),
-            lr=config.lr,
+            lr=config.base_lr,
             # weight_decay=getattr(config, "weight_decay", 0.0)
         )
 
     if config.type == "sgd":
         return torch.optim.SGD(
             model.parameters(),
-            lr=config.lr
+            lr=config.base_lr
         )
 
     if config.type == "adamw":
         return torch.optim.AdamW(
             model.parameters(),
-            lr=config.lr,
+            lr=config.base_lr,
             # weight_decay=getattr(config, "weight_decay", 0.0)
+        )
+
+    if config.type == "radam":
+        return torch.optim.RAdam(
+            model.parameters(),
+            lr=config.base_lr
         )
 
     raise ValueError(f"Unknown optimizer: {config.type}")
